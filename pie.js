bed6bba7340b08c60d4931b59923cdea5e762e79
Sergio Gimenez <gimenezsergio123@gmail.com>
2022-09-28 14:06:11 -0300

    Agregamos el footer de forma dinamica

---
+++ pie.js
@@ -0,0 +1,46 @@
+miPie = /*html*/`
+<div class="col mb-3">
+<a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+  <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+</a>
+<p class="text-muted">© 2023</p>
+</div>
+
+<div class="col mb-3">
+
+</div>
+
+<div class="col mb-3">
+<h5>Section</h5>
+<ul class="nav flex-column">
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+</ul>
+</div>
+
+<div class="col mb-3">
+<h5>Section</h5>
+<ul class="nav flex-column">
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+</ul>
+</div>
+
+<div class="col mb-3">
+<h5>Section</h5>
+<ul class="nav flex-column">
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+</ul>
+</div>
+`
+document.querySelector('footer').innerHTML = miPie
